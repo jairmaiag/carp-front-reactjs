1b726c126f244ecea4aad21a6c3a414e4d6dfc1d
jairmaiag <jairmaiag@gmail.com>
2020-06-23 15:20:12 -0300

    Criação do componente de Home

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+//import { Row, Col, Card, Icon } from "react-materialize";
+
+export default class Home extends Component {
+  render() {
+    return <div>Home</div>;
+  }
+}
